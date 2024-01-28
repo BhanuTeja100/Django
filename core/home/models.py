c03463d6a83598cd09b244f6dfa63a6ca0513df0
Bhanu_Teja <ombhanu100@gmail.com>
2024-01-28 13:04:58 +0530

    creating models and doing migrations

--- core/home/models.py
+++ core/home/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+# we create our database models here for this app
+
+# Let's create Student Model
+
+
+
+# Structure of a model
+class Student(models.Model):
+    # id = models.AutoField()   # This is automatically added by django which is primary key 
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField(null=True, blank=True)
+    image = models.ImageField(blank=True)
+    file = models.FileField()
+ 
+class Product(models.Model):
+    pass
